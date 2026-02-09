3543b33e63ffd6518a6cca0161e52c10e07265a9
andom-0001 <anandkumar241103@gmail.com>
2026-02-09 14:30:11 +0530

    projectz

--- e-learning/index.js
+++ e-learning/index.js
@@ -5,3 +5,19 @@ const port = 8080;
 
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'ui'));
+
+app.get('/',(req,res)=>{
+    res.render('index',{
+        title:'HOME_PAGE'
+    })
+});
+
+app.get('/about',(req,res)=>{
+    res.render('about',{
+        title:'ABOUT_US'
+    })
+});
+
+app.listen(port,()=>{
+    console.log(`server is running on port ${port}`);
+})
